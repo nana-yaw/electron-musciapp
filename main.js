28b88c0cb7f60fc2da481e487cb42c88aeb1e2d6
edwardosei <edward.osei-nyarko@turntabl.io>
2022-09-16 14:01:44 +0000

    Setup project with UI

---
+++ main.js
@@ -0,0 +1,26 @@
+const {app, BrowserWindow} = require('electron')
+const path = require('path')
+
+const createWindow = () => {
+    let mainWindow = new BrowserWindow({
+        width: 400,
+        height: 500,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js')
+        }
+    })
+
+    mainWindow.loadFile('index.html')
+}
+
+app.whenReady().then(() => {
+    createWindow()
+
+    app.on('activate', function () {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    })
+})
+
+app.on('window-all-closed', function () {
+    if (process.platform !== 'darwin') app.quit()
+})
